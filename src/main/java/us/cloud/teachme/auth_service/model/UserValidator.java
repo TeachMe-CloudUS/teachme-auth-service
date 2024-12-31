da8cf8734987ba516d4f5e9e55f963a84b24a89b
Juangr4 <jgarciaruiz2001@gmail.com>
2024-12-31 02:54:58 +0100

    refactor: add @NonNull annotations to method parameters in JwtAuthenticationFilter and UserValidator

--- src/main/java/us/cloud/teachme/auth_service/model/UserValidator.java
+++ src/main/java/us/cloud/teachme/auth_service/model/UserValidator.java
@@ -1,5 +1,6 @@
 package us.cloud.teachme.auth_service.model;
 
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
@@ -9,20 +10,20 @@ import org.springframework.validation.Validator;
 public class UserValidator implements Validator {
 
   @Override
-  public boolean supports(Class<?> clazz) {
+  public boolean supports(@NonNull Class<?> clazz) {
     return User.class.isAssignableFrom(clazz);
   }
 
   @Override
-  public void validate(Object target, Errors errors) {
+  public void validate(@NonNull Object target, @NonNull Errors errors) {
     ValidationUtils.rejectIfEmpty(errors, "email", "email.empty", "The email can't be null");
     User user = (User) target;
-    if(user.getEmail().length() <= 3 || user.getEmail().length() > 60) {
+    if (user.getEmail().length() <= 3 || user.getEmail().length() > 60) {
       errors.rejectValue("email", "email.length", "The email length must be between 4 and 60 characters");
     }
-    if(!user.getEmail().matches("^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$")) {
+    if (!user.getEmail().matches("^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$")) {
       errors.rejectValue("email", "email.invalid", "The email is invalid");
     }
   }
-  
+
 }
