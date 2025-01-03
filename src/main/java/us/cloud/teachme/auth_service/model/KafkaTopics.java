ae75974644dd068ea589808cb229f072bc84de3e
Juangr4 <jgarciaruiz2001@gmail.com>
2025-01-03 17:27:48 +0100

    refactor: update Kafka topics structure to follow group's standart

--- src/main/java/us/cloud/teachme/auth_service/model/KafkaTopics.java
+++ src/main/java/us/cloud/teachme/auth_service/model/KafkaTopics.java
@@ -1,19 +1,25 @@
 package us.cloud.teachme.auth_service.model;
 
 public enum KafkaTopics {
-  USER_CREATED("user-created"),
-  USER_UPDATED("user-updated"),
-  USER_DELETED("user-deleted"),
-  USER_ACTIVATED("user-activated"),
-  MAIL_SEND("mail-send");
+  USER_CREATED("auth-service", "user", "created"),
+  USER_UPDATED("auth-service", "user", "updated"),
+  USER_DELETED("auth-service", "user", "deleted"),
+  USER_ACTIVATED("auth-service", "user", "activated"),
+  MAIL_SEND("auth-service", "mail", "send");
 
-  private final String topic;
+  private final String service;
 
-  KafkaTopics(String topic) {
-    this.topic = topic;
+  private final String entity;
+
+  private final String action;
+
+  KafkaTopics(String service, String entity, String action) {
+    this.service = service;
+    this.entity = entity;
+    this.action = action;
   }
 
   public String getTopic() {
-    return topic;
+    return service + "." + entity + "." + action;
   }
 }
