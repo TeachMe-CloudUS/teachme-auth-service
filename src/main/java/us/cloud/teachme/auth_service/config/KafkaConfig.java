67c14bb51653ddb83405799f9776195197ca416d
Juangr4 <jgarciaruiz2001@gmail.com>
2025-01-05 21:17:44 +0100

    fix: update Kafka producer value serializer to JsonSerializer

--- src/main/java/us/cloud/teachme/auth_service/config/KafkaConfig.java
+++ src/main/java/us/cloud/teachme/auth_service/config/KafkaConfig.java
@@ -13,6 +13,7 @@ import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import us.cloud.teachme.auth_service.model.KafkaTopics;
 
@@ -58,7 +59,7 @@ public class KafkaConfig {
     return Map.of(
         ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers,
         ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class,
-        ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
   }
 
   @Bean
